ace2876df0bf2338db1661c4ccda0252487a8f80
benoitlx <benoitleroux41@gmail.com>
2023-10-24 15:04:05 +0200

    automata

--- lib/Automata/Automata.cpp
+++ lib/Automata/Automata.cpp
@@ -1,2 +1,14 @@
 #include "Automata.h"
 #include "Arduino.h"
+
+void automataInit(){
+  current_state = WaitPush;
+}
+
+enum states getCurrentSate(){
+  return current_state;
+}
+
+void updateState(int event){
+  current_state = automata[event][current_state];
+}
--- lib/Automata/Automata.h
+++ lib/Automata/Automata.h
@@ -1,8 +1,30 @@
 #ifndef AUTOMATA_H
 #define AUTOMATA_H
 
+#define N_STATE 5
+#define N_TRANS 6
+
+
+enum Event{ButtonPush, ObstacleDetected, ObstacleAvoided, RobotRight, RobotLeft, EndEvent}; // Transition of the automata
+
 enum states{Follow, Avoid, Stop, WaitPush, End};
 extern enum states current_state;
+
+extern enum states automata[N_TRANS][N_STATE] = {
+  // Follow, Avoid, Stop, WaitPush, End
+  {Follow, Avoid, Stop, Follow, End},
+  {Avoid, Avoid, Stop, WaitPush, End},
+  {Follow, Follow, Stop, WaitPush, End},
+  {Stop, Avoid, Stop, WaitPush, End},
+  {Follow, Avoid, Follow, WaitPush, End},
+  {End, Avoid, Stop, WaitPush, End}
+};
+
+
+
+void automataInit();
+enum states getCurrentState();
+void updateState(Event event);
 
 #endif // !AUTOMATA_H
 
